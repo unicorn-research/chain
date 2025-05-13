e561e3445a0b5f8d8e0cd619252dddf86f78f118
Jacob Gadikian <jacobgadikian@gmail.com>
2025-05-13 18:01:03 +0700

    add daemon folder

---
+++ chaind/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"cosmossdk.io/log"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	simapp "github.com/unicorn-research/chain"
+	"github.com/unicorn-research/chain/chaind/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", simapp.DefaultNodeHome); err != nil {
+		log.NewLogger(rootCmd.OutOrStderr()).Error("failure when running app", "err", err)
+		os.Exit(1)
+	}
+}
